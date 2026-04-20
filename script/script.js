7cd3ceb39b7a82729666ec75a06ba239321f9acc
lessionzyx <lession.zyx@gmail.com>
2026-04-20 09:50:49 -0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,174 @@
+const buttons = document.querySelectorAll(".toggle-btn");
+const imgs = document.querySelectorAll(".img-btn")
+
+buttons.forEach(button => {
+
+	button.addEventListener("click", () => { 
+
+		const target = document.getElementById(button.dataset.target);
+		const isOpen = target.classList.contains("show");
+
+		document.querySelectorAll(".content").forEach(el => {
+			el.classList.remove("unshow");
+		});
+
+		document.querySelectorAll(".toggle-btn").forEach(btn => {
+			btn.classList.add("unshow");
+		});
+
+		if(isOpen) {
+
+			if (button.dataset.target == "subDesc") {
+				document.getElementById("img2").classList.remove("unshow");
+				document.getElementById("title").classList.remove("unshow");
+				document.getElementById("title2").classList.remove("unshow");
+				document.getElementById("title1").classList.remove("unshow");
+
+				document.querySelectorAll(".toggle-btn").forEach(btn => {
+					btn.classList.remove("unshow");
+				});
+				button.classList.remove("bx-list-minus");
+				button.classList.add("bx-list-plus");	
+
+				target.classList.remove("show");
+
+			} else {
+
+				document.getElementById("img1").classList.remove("unshow");
+				document.getElementById("title").classList.remove("unshow");
+				document.getElementById("title1").classList.remove("unshow");
+				document.getElementById("title2").classList.remove("unshow");
+
+				document.querySelectorAll(".toggle-btn").forEach(btn => {
+					btn.classList.remove("unshow");
+				});
+				button.classList.remove("bx-list-minus");
+				button.classList.add("bx-list-plus");
+
+				target.classList.remove("show");
+
+			}
+
+		} else {
+
+			if (button.dataset.target == "subDesc") {
+		
+				document.getElementById("img2").classList.add("unshow");
+				document.getElementById("title").classList.add("unshow");
+				document.getElementById("title2").classList.add("unshow");
+				document.getElementById("title1").classList.add("unshow");
+
+				button.classList.remove("bx-list-plus");
+				button.classList.add("bx-list-minus");
+				button.classList.remove("unshow");
+
+				target.classList.add("show");
+	
+			} else {
+
+				document.getElementById("img1").classList.add("unshow");
+				document.getElementById("title").classList.add("unshow");
+				document.getElementById("title1").classList.add("unshow");
+				document.getElementById("title2").classList.add("unshow");
+
+				button.classList.remove("bx-list-plus");
+				button.classList.add("bx-list-minus");
+				button.classList.remove("unshow");
+
+				target.classList.add("show");
+
+			}
+
+		}
+
+	});	
+});
+
+imgs.forEach(img => {
+
+	img.addEventListener("click", () => { 
+
+		const target = document.getElementById(img.dataset.target);
+		const isOpen = target.classList.contains("show");
+		var button = (img.dataset.target =="subDesc") ? document.getElementById("btn1"):document.getElementById("btn2");
+
+		document.querySelectorAll(".content").forEach(el => {
+			el.classList.remove("unshow");
+		});
+
+		document.querySelectorAll(".toggle-btn").forEach(btn => {
+			btn.classList.add("unshow");
+		});
+
+		if(isOpen) {
+
+			if (img.dataset.target == "subDesc") {
+				document.getElementById("img2").classList.remove("unshow");
+				document.getElementById("title").classList.remove("unshow");
+				document.getElementById("title2").classList.remove("unshow");
+				document.getElementById("title1").classList.remove("unshow");
+
+				document.querySelectorAll(".toggle-btn").forEach(btn => {
+					btn.classList.remove("unshow");
+				});
+				button.classList.remove("bx-list-minus");
+				button.classList.add("bx-list-plus");	
+
+				target.classList.remove("show");
+
+			} else {
+
+				document.getElementById("img1").classList.remove("unshow");
+				document.getElementById("title").classList.remove("unshow");
+				document.getElementById("title1").classList.remove("unshow");
+				document.getElementById("title2").classList.remove("unshow");
+
+				document.querySelectorAll(".toggle-btn").forEach(btn => {
+					btn.classList.remove("unshow");
+				});
+				button.classList.remove("bx-list-minus");
+				button.classList.add("bx-list-plus");
+
+				target.classList.remove("show");
+
+			}
+
+		} else {
+
+			if (img.dataset.target == "subDesc") {
+		
+				document.getElementById("img2").classList.add("unshow");
+				document.getElementById("title").classList.add("unshow");
+				document.getElementById("title2").classList.add("unshow");
+				document.getElementById("title1").classList.add("unshow");
+
+				button.classList.remove("bx-list-plus");
+				button.classList.add("bx-list-minus");
+				button.classList.remove("unshow");
+
+				target.classList.add("show");
+	
+			} else {
+
+				document.getElementById("img1").classList.add("unshow");
+				document.getElementById("title").classList.add("unshow");
+				document.getElementById("title1").classList.add("unshow");
+				document.getElementById("title2").classList.add("unshow");
+
+				button.classList.remove("bx-list-plus");
+				button.classList.add("bx-list-minus");
+				button.classList.remove("unshow");
+
+				target.classList.add("show");
+
+			}
+
+		}
+
+	});	
+});
+
+
+
+
+
